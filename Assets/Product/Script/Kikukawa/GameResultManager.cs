0a915f3a115c210340a6f048cb6a54ec7c6dc53b
KikukawaMakoto <49179651+KikukawaMakoto@users.noreply.github.com>
2024-04-29 12:16:48 +0900

    ・Result調整中

--- Assets/Product/Script/Kikukawa/GameResultManager.cs
+++ Assets/Product/Script/Kikukawa/GameResultManager.cs
@@ -8,17 +8,18 @@ using UnityEngine;
 
 namespace Kikukawa {
     public class GameResultManager : SingletonMonoBehaviour<GameResultManager> {
-        [SerializeField] GameObject m_Inochi_Anim;
+        [SerializeField] GameObject m_GameClearObj;
+        [SerializeField] GameObject m_GameOverObj;
         [SerializeField] bool m_GameClearFlag = false;
 
         void Start() {
             if (m_GameClearFlag) {
-
+                Manager.FadeManager.Instance.SetFadeColor(new Color(0.0f, 0.0f, 0.0f, 1.0f));
+                Manager.FadeManager.Instance.SetFadeFlag(false);
+                m_GameClearObj.SetActive(true);
             } else {
                 Manager.FadeManager.Instance.SetFadeColor(new Color(0.0f, 0.0f, 0.0f, 0.0f));
-                if (m_Inochi_Anim) {
-                    m_Inochi_Anim.SetActive(true);
-                }
+                m_GameOverObj.SetActive(true);
             }
 
         }
